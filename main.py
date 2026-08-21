d307483a7a7bb7376befc5618c7a05bde46f29d4
narongsak14a <narongsak14@gmail.com>
2026-08-21 19:06:19 +0700

    Modify Google Sheet CSV URL for asset fetching
    Updated Google Sheet CSV URL to include specific gid for fetching data.

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ PORTFOLIO_URL = "https://broad-disk-2905.narongsak14.workers.dev/"
 YOUTUBE_VIDEO_IDS = [""]
 
 # ใส่ URL ที่ได้จากการ Publish Google Sheet เป็น CSV ตรงนี้
-GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSyU-ww2M22WZq781y1pEwbXihk0iOar0xyx2ZIo776WgbdQ0kXAK-9S6ckGgHk3F7N1BGsaqDv4zwR/pub?output=csv"
+GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSyU-ww2M22WZq781y1pEwbXihk0iOar0xyx2ZIo776WgbdQ0kXAK-9S6ckGgHk3F7N1BGsaqDv4zwR/pub?gid=1067250335&single=true&output=csv"
 # ==========================================
 # ฟังก์ชันดึง ASSETS จาก Google Sheet
 # ==========================================
